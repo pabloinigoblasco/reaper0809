b48119cee8ed2882ccb38ef45cde2eccd7f31181
rmburrueco <rmburrueco@fa35cb9e-84e2-11de-84a8-8de474f20c78>
2009-08-28 18:46:49 +0000

    Más cositas...

---  reaper0809 --username rmburrueco/Codigo/Reaper/org/lsi/us/es/reaper/Core/Configurations.java
+++  reaper0809 --username rmburrueco/Codigo/Reaper/org/lsi/us/es/reaper/Core/Configurations.java
@@ -11,7 +11,7 @@ public class Configurations {
 	public static final String FormModelMappingFile ="formmodel-mapping.xml";
 	public static final String QueryModelMappingFile ="querymodel-mapping.xml";
 	
-	public static final String SeleniumNavigator="*firefox";
+	public static final String SeleniumNavigator="opera";
 	public static final String FormFillerStrategy="org.lsi.us.es.reaper.SeleniumFormFillerStrategy.SeleniumFillerApplication";
 	public static final String submitWaitMilliseconds="30000";
 	public static final String OutputDirectory="./Results/";
